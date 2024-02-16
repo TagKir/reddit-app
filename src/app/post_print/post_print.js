7c36b44b3d596b1a88aebb31df98334cca59d8e1
TagKir <tagirkiky@gmail.com>
2024-02-16 22:35:09 +0300

    modified 4 files

--- src/app/post_print/post_print.js
+++ src/app/post_print/post_print.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./post_print.css";
 
 export default function Post({ post }) {
-  console.log(post);
+  const imageUrlRegex = /\.(jpeg|jpg|gif|png)$/i;
+
+  function calculateDaysDifference(unixTime) {
+    const now = new Date();
+    const pastDate = new Date(unixTime * 1000);
+    const differenceInMilliseconds = now - pastDate;
+    const differenceInHours = Math.floor(
+      differenceInMilliseconds / (1000 * 60 * 60)
+    );
+    return differenceInHours;
+  }
+
   return (
     <div className="post" key={post.id}>
       <div className="top">
@@ -15,20 +26,26 @@ export default function Post({ post }) {
             <i class="fa-solid fa-arrow-down"></i>
           </li>
         </ul>
-        <h2 className="title">{post.title}</h2>
+        <h2 className={imageUrlRegex.test(post.url) ? "title" : "main_title"}>
+          {post.title}
+        </h2>
       </div>
 
-      {post.preview ? <img src={post.url} className="shrek" /> : <h1>hello</h1>}
+      {imageUrlRegex.test(post.url) ? (
+        <img src={post.url} className="shrek" />
+      ) : (
+        ""
+      )}
 
       <div className="nonmain_inf">
         <p>
           posted by <a href="http://randomcolour.com">{post.author}</a>
         </p>
-        <p>1 hour age</p>
+        <p>{calculateDaysDifference(post.created)} Hours age</p>
         <div className="comments">
           <i class="fa-regular fa-comments"></i>
           <p>
-            <a href="http://randomcolour.com">{post.numcomments}</a>
+            <a href="http://randomcolour.com">{post.num_comments}</a>
           </p>
         </div>
       </div>
